29fda2e0a3bc20e567089281d7cde79df4c21899
mochi-sann <44772513+mochi-sann@users.noreply.github.com>
2021-12-29 15:53:34 +0900

    更新

--- app/core/layouts/Layout.tsx
+++ app/core/layouts/Layout.tsx
@@ -1,5 +1,5 @@
 import { Head, BlitzLayout, Link } from "blitz"
-import { Container, Stack, Box, Heading } from "@chakra-ui/react"
+import { Container, Stack, Text, Box, Heading, Flex } from "@chakra-ui/react"
 import React from "react"
 type LayoutProps = {
   title: string
@@ -8,22 +8,34 @@ type LayoutProps = {
 const Layout: BlitzLayout<LayoutProps> = ({ title, children }: LayoutProps) => {
   const ContainerMaxW = "800px"
   return (
-    <>
+    <Box h="100vh">
       <Head>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <Flex h="100%" flexDirection="column">
+        <Box borderBottomWidth="2px">
+          <Container maxW={ContainerMaxW}>
+            <Stack p="2" spacing="2" direction="row">
+              <Heading>
+                <Link href="/">{"ppeage" + title}</Link>
+              </Heading>
+            </Stack>
+          </Container>
+        </Box>
 
-      <Box borderBottomWidth="2px">
-        <Container maxW={ContainerMaxW}>
-          <Stack p="2" spacing="2" direction="row">
-            <Heading>
-              <Link href="/">{"ppeage" + title}</Link>
-            </Heading>
-          </Stack>
+        <Container maxW={ContainerMaxW} flex="1" h="100%">
+          {children}
         </Container>
-      </Box>
-      <Container maxW={ContainerMaxW}>{children}</Container>
-    </>
+
+        <Box as="footer" borderTopWidth="2px">
+          <Container maxW={ContainerMaxW}>
+            <Stack p="2" spacing="2" direction="row">
+              <Text>テキスト </Text>
+            </Stack>
+          </Container>
+        </Box>
+      </Flex>
+    </Box>
   )
 }
 
--- app/notes/mutations/createNote.ts
+++ app/notes/mutations/createNote.ts
@@ -3,7 +3,8 @@ import db from "db"
 import { z } from "zod"
 
 const CreateNote = z.object({
-  name: z.string(),
+  title: z.string(),
+  body: z.string(),
 })
 
 export default resolver.pipe(resolver.zod(CreateNote), resolver.authorize(), async (input) => {
